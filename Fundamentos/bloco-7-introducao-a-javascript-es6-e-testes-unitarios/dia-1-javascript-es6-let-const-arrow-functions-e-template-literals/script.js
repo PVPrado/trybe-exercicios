92436536511471c7295b465f4f859eb7c820f664
Paulo Victor <paulo.v.prado@hotmail.com>
2022-06-23 18:37:15 -0300

    ver oq e

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,34 @@
+// Exercicio 1 pt.1
+
+// const testingScope = (escopo) => (escopo === true) ? ` ótimo, fui utilizada no escopo !` : `Não devo ser utilizada fora meu escopo (else)`;
+// console.log(testingScope());
+
+// function testingScope(escopo) {
+// if (escopo === true) {
+//   var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//   ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//    console.log(ifScope);
+//  } else {
+//    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//    console.log(elseScope);
+// }
+// console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+
+//  testingScope(true);
+
+// Exercicio 2 pt.1
+
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// oddsAndEvens.sort((a, b ) => a - b)
+
+// console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+// PARTE 2
+
+// const fatorial = (N) => {
+  
+
+
+// }
