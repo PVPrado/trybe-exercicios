bccbf8df443664f25726d65baebbfa93af9ff0cb
Paulo Victor <paulo.v.prado@hotmail.com>
2022-07-19 15:12:09 -0300

    exercicios concluidos

---
+++ Front-End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/my-app2/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className='footer'>
+        <h1>E isso é só o começo...</h1>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
